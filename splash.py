8ddc6d6585237e5e9e428c45ed3e5dc5f7038d27
Gabriel Henrique <gabriel.ferreira@ar.com.br>
2022-10-19 11:07:05 -0300

    Ajustes na tela de splash

--- splash.py
+++ splash.py
@@ -1,5 +1,6 @@
 from tkinter import*
 from tkinter import Tk
+from tkinter import messagebox
 from tkinter.ttk import Progressbar
 
 janela = Tk()
@@ -17,8 +18,11 @@ janela.geometry("%dx%d+%d+%d" % (largura,altura,posx,posy))
 janela.configure(bg="white")
 janela.resizable(0,0)
 
+def show():
+    messagebox.showinfo("Sucesso","Carregamento concluído")
+
 def bar():
-    l4 = Label(janela,text="Carregando aplicação...",fg="green",bg="white")
+    l4 = Label(janela,text="Carregando aplicação...",fg="DarkGreen",bg="white")
     l4.place(x=0,y=210)
     import time
     r=0
@@ -28,11 +32,19 @@ def bar():
         time.sleep(0.03)
         r=r+1
     janela.destroy()
+    show()
 
 progress = Progressbar(janela,orient=HORIZONTAL,length=500,mode='determinate')
 progress.place(x=-10,y=235)
 
-btn = Button(janela, width=10, height=1, text="Abrir", command=bar)
-btn.place(x=170,y=200)
+lbl_title = Label(janela,text="SmartAgro",font=("Ivy 20 bold"),fg="DarkGreen",bg="white")
+lbl_title.place(x=140,y=80)
+
+lbl_subtitle = Label(janela,text="Soluções Agrônomo Inteligente",font=("Ivy 10 bold"),fg="DarkGreen",bg="white")
+lbl_subtitle.place(x=110,y=115)
+
+btn = Button(janela, width=10, height=1, text="Acessar", 
+    command=bar, relief='flat', bg="DarkGreen", fg='white',font=("Ivy 10 bold"))
+btn.place(x=170,y=180)
 
 janela.mainloop()
