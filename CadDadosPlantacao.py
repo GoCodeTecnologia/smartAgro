0bcb5a997e7be6749119b9c9ad67642886203864
GoCodeTecnologia <104987625+GoCodeTecnologia@users.noreply.github.com>
2022-10-27 15:12:18 -0300

    versao-2710-4.2

--- CadDadosPlantacao.py
+++ CadDadosPlantacao.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 from DcCommand import*
 
 def cadDadosPlantacao():
-    janela = Toplevel()
+    janela = Tk()
     janela.title("SmartAgro - Emissão CO² Cadastro de Dados")
     global tree
 
@@ -25,50 +25,44 @@ def cadDadosPlantacao():
     janela.configure(bg="white")
     janela.resizable(0,0)
 
-    def calcEmissaoFazenda():
+    def calcEmissaoPlantacao():
         try:
-            nome = input_nm_local.get().strip()
-            tipo = input_tipo_consumo.get()
-            mes = int(input_qtd_mes.get())
-            valor = float(input_valor_consumo.get())
+            tipo_fertilizante = input_fertilizante.get()
+            quilo = float(input_quilo.get())
+            quilo_dolomito = float(input_qtd_dolomito.get())
+            area = float(input_area.get())
+            sementes = float(input_qtd_sementes.get())
 
-            if mes <0 or valor <0 or nome == "":
-                messagebox.showerror("Erro","Insira uma quantidade válida")
-                lbl_resultado_carbono.config(text="Dados Incorretos")
-                return
-            elif mes >0 or valor >0:
-                
-                co2Kwh   = 0.1355
-                co2Reais = co2Kwh / 0.347
+            if tipo_fertilizante == "Calcário":
+            # Calcario
+                fatorCalcario = 0.12
+                fatorDolomitico = 0.13
 
-                if (tipo == "Dinheiro"):
-                    value = valor
-                    valueTratado = value
-                    if (valueTratado < 100):
-                        result = (valueTratado * co2Reais)
-                        resultadoEnergia = ((valueTratado * co2Reais) * mes)
-                    else:
-                        result = (valueTratado * co2Reais)
-                        resultadoEnergia = (((valueTratado * co2Reais) * mes) / 100)
-                    total_emissao = round(resultadoEnergia,2)
-                        
-                elif (tipo == "Kwh"):
-                    value = valor
-                    result = (value * co2Kwh)
-                    resultadoEnergia = ((value * co2Kwh) * mes)
-                    total_emissao = round(resultadoEnergia, 2)
-                    
-            lista_inserir = [nome, tipo, mes, valor, total_emissao]
+                resultado = ((quilo * fatorCalcario + quilo_dolomito * fatorDolomitico) * 44 / 12)
+
+            elif tipo_fertilizante == "Uréia":
+            # Ureia
+                quilo_dolomito = 0
+                fatorUreia = 0.20
+                resultado = (quilo * fatorUreia * 44 / 12)
+
+            fatorSemente = 0.92
+            resultadoSemente = (area * sementes * fatorSemente)
+
+            total_emissao = round((resultado + resultadoSemente),4)
+
+            lista_inserir = [tipo_fertilizante, quilo, quilo_dolomito, area, sementes, total_emissao]
 
             for i in lista_inserir:
                 if i=='':
                     messagebox.showerror("Erro","Dados vazios")
                     return
 
-            inserir_dados_fazenda(lista_inserir)
-            mostrar()
+            inserir_dados_plantacao(lista_inserir)
+            mostrar()             
         except:
             messagebox.showerror("Erro","Dados inválidos ou não informados")
+            janela.destroy()
 
     janela.defaultFont = font.nametofont("TkDefaultFont")
     janela.defaultFont.configure(family="Nunito Sans", 
@@ -90,29 +84,37 @@ def cadDadosPlantacao():
 
     # ==========================================================================================================
     # Formulário e Inputs
-    lbl_nm_local = Label(boxBtn,text="Nome do Local",font=("Nunito 10 bold"),fg=principal,bg="white")
-    lbl_nm_local.place(x=20,y=10)
-    input_nm_local = Entry(boxBtn,width=30,highlightthickness=1,relief=FLAT)
-    input_nm_local.place(x=20,y=40)
-    input_nm_local.config(highlightbackground=principal, highlightcolor=principal)
+    lstTipo = ["Calcário","Uréia"]
+    lbl_input_fertilizante = Label(boxBtn,text="Tipo do Fertilizante",font=("Nunito 10 bold"),fg=principal,bg="white")
+    lbl_input_fertilizante.place(x=20,y=10)
+    input_fertilizante = ttk.Combobox(boxBtn, width=25, values=lstTipo, state="readonly")
+    input_fertilizante.place(x=20,y=40)
 
-    lstTipo = ["Dinheiro","Kwh"]
-    lbl_tipo_consumo = Label(boxBtn,text="Escolha o tipo de consumo",font=("Nunito 10 bold"),fg=principal,bg="white")
-    lbl_tipo_consumo.place(x=250,y=10)
-    input_tipo_consumo = ttk.Combobox(boxBtn, width=25, values=lstTipo, state="readonly")
-    input_tipo_consumo.place(x=250,y=40)
+    lbl_quilo = Label(boxBtn,text="Quantidade de kilos",font=("Nunito 10 bold"),fg=principal,bg="white")
+    lbl_quilo.place(x=250,y=10)
+    input_quilo = Entry(boxBtn,width=25,highlightthickness=1,relief=FLAT)
+    input_quilo.place(x=250,y=40)
+    input_quilo.config(highlightbackground=principal, highlightcolor=principal)
 
-    lbl_valor_consumo = Label(boxBtn,text="Valor (Kwh ou R$)",font=("Nunito 10 bold"),fg=principal,bg="white")
-    lbl_valor_consumo.place(x=450,y=10)
-    input_valor_consumo = Entry(boxBtn,width=30,highlightthickness=1,relief=FLAT)
-    input_valor_consumo.place(x=450,y=40)
-    input_valor_consumo.config(highlightbackground=principal, highlightcolor=principal)
+    lbl_qtd_dolomito = Label(boxBtn,text="Quantidade de dolomito",font=("Nunito 10 bold"),fg=principal,bg="white")
+    lbl_qtd_dolomito.place(x=450,y=10)
+    lbl_qtd_dolomito = Label(boxBtn,text="*Quando CALCÁRIO insira 0",font=("Nunito 8 bold"),fg=secundaria,bg="white")
+    lbl_qtd_dolomito.place(x=450,y=60)
+    input_qtd_dolomito = Entry(boxBtn,width=25,highlightthickness=1,relief=FLAT)
+    input_qtd_dolomito.place(x=450,y=40)
+    input_qtd_dolomito.config(highlightbackground=principal, highlightcolor=principal)
 
-    lbl_qtd_mes = Label(boxBtn,text="Quantidade de meses",font=("Nunito 10 bold"),fg=principal,bg="white")
-    lbl_qtd_mes.place(x=650,y=10)
-    input_qtd_mes = Entry(boxBtn,width=30,highlightthickness=1,relief=FLAT)
-    input_qtd_mes.place(x=650,y=40)
-    input_qtd_mes.config(highlightbackground=principal, highlightcolor=principal)
+    lbl_area = Label(boxBtn,text="Área Total",font=("Nunito 10 bold"),fg=principal,bg="white")
+    lbl_area.place(x=650,y=10)
+    input_area = Entry(boxBtn,width=20,highlightthickness=1,relief=FLAT)
+    input_area.place(x=650,y=40)
+    input_area.config(highlightbackground=principal, highlightcolor=principal)
+
+    lbl_qtd_sementes = Label(boxBtn,text="Qtd. Sementes",font=("Nunito 10 bold"),fg=principal,bg="white")
+    lbl_qtd_sementes.place(x=800,y=10)
+    input_qtd_sementes = Entry(boxBtn,width=15,highlightthickness=1,relief=FLAT)
+    input_qtd_sementes.place(x=800,y=40)
+    input_qtd_sementes.config(highlightbackground=principal, highlightcolor=principal)
 
     lbl_resultado_carbono = Label(boxBtn,font=("Nunito 10 bold"),fg=textBlue,bg="white")
     lbl_resultado_carbono.place(x=20,y=70)
@@ -121,8 +123,8 @@ def cadDadosPlantacao():
     # Tabela e botão cadastrar
     def mostrar():
         # Criando a tabela
-        tabela_head = ['Cód.','Nome Local','Tipo (kwh/R$)','Total (kwh/R$)','Meses','Emissão CO²']
-        lista_itens = ver_tabela_fazenda()
+        tabela_head = ['Cód.','Fertilizante','Quilos (Kgs)','Kgs Dolomita','Área','Total de Sementes','Emissão CO²']
+        lista_itens = ver_tabela_plantacao()
 
         tree = ttk.Treeview(boxBtn, selectmode="extended",
                                 columns=tabela_head, show="headings")
@@ -133,7 +135,7 @@ def cadDadosPlantacao():
         boxBtn.grid_rowconfigure(0, weight=10)
 
         hd = ["nw","nw","nw"]
-        h = [20,100,100]
+        h = [10,80,80]
         n=0
 
         for col in tabela_head:
@@ -146,7 +148,7 @@ def cadDadosPlantacao():
     mostrar()
 
     btn_calcular = Button(boxBtn,width=22, height=1, text="Cadastrar Dados", relief='flat',
-            bg=secundaria, fg='white',font=("Nunito 15 bold"),command=calcEmissaoFazenda)
+            bg=secundaria, fg='white',font=("Nunito 15 bold"),command=calcEmissaoPlantacao)
     btn_calcular.place(x=20,y=360)
 
     lbl_credits = Label(janela,text="GoCode Tecnologia, 2022",fg=secundaria,bg="white")
